49993bb93744b03271395d8d31edb543bb89b196
Kenson Johnson <kenson.johnson@outlook.com>
2023-10-17 15:51:22 -0500

    Added minimum components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+export const metadata = {
+  title: "Crypto Real Time Inference",
+  description: "Built by Chris Keim",
+};
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
-    </main>
-  );
+  return <></>;
 }

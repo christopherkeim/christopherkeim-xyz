90ae98c5953564aee205994b2df79f10b87b4ae8
Kenson Johnson <kenson.johnson@outlook.com>
2023-11-02 09:49:21 -0500

    Added sample response in comments

--- components/Projects/Crypto/PredictionContainer.tsx
+++ components/Projects/Crypto/PredictionContainer.tsx
@@ -7,6 +7,19 @@ import {
 } from "@/components/Projects/Crypto/PredictionForm";
 import { PredictionResult } from "@/components/Projects/Crypto/PredictionResult";
 
+// Example prediction response:
+// {
+//   "prediction": {
+//     "model": "cnn",
+//     "coin": "BTC-USD",
+//     "current_price": "34433.76",
+//     "prediction": "35680.61",
+//     "difference": "+1246.85",
+//     "time": "1698894000000",
+//     "request_timestamp": "1698894259695"
+//   }
+// }
+
 export function PredictionContainer() {
   const [prediction, setPrediction] = useState<Prediction | null>(null);
 

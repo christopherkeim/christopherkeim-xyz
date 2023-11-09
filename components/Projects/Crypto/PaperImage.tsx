d287e4241723b3abf2b997ff4ae6fd692a1f64f7
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-09 13:10:24 -0600

    Defined aspect ratio

--- components/Projects/Crypto/PaperImage.tsx
+++ components/Projects/Crypto/PaperImage.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 
 export function PaperImage({ src, alt }: { src: string; alt?: string }) {
   return (
-    <div className="relative w-full">
-      <Image
-        className="my-2 block w-full rounded-sm object-contain shadow-md dark:shadow-sm dark:shadow-slate-600"
-        src={src}
-        fill
-        priority
-        alt={alt ?? ""}
-      />
-    </div>
+    <Image
+      className="my-2 block rounded-sm shadow-md dark:shadow-sm dark:shadow-slate-600"
+      src={src}
+      width={768}
+      height={432}
+      priority
+      alt={alt ?? ""}
+    />
   );
 }

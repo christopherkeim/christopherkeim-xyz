5a2c9f9175e7ac790bf24f2e20599c5ea98e2fd1
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-09 13:06:21 -0600

    Added alt prop and dynamic fill

--- components/Projects/Crypto/PaperImage.tsx
+++ components/Projects/Crypto/PaperImage.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
-export function PaperImage({ src }: { src: string }) {
+export function PaperImage({ src, alt }: { src: string; alt?: string }) {
   return (
-    <Image
-      className="my-2 block rounded-sm shadow-md dark:shadow-sm dark:shadow-slate-600"
-      src={src}
-      width={768}
-      height={432}
-      priority
-      alt="404"
-    />
+    <div className="relative w-full">
+      <Image
+        className="my-2 block w-full rounded-sm object-contain shadow-md dark:shadow-sm dark:shadow-slate-600"
+        src={src}
+        fill
+        priority
+        alt={alt ?? ""}
+      />
+    </div>
   );
 }

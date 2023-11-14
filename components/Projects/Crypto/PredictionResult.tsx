696097e65e92ce81aca6af1727b5e5d11d127714
Christopher Keim <christopher.jl.keim@gmail.com>
2023-11-13 21:07:59 -0500

    Changes for mobile and desktop
    Added prediction result title that displays either Bitcoin or Ethereum based on requested prediction.
    
    Added top padding between prediction form and prediction result for mobile.
    
    Added border and deleted shadow.

--- components/Projects/Crypto/PredictionResult.tsx
+++ components/Projects/Crypto/PredictionResult.tsx
@@ -8,44 +8,36 @@ type PredictionResultProps = {
 
 export function PredictionResult({ prediction }: PredictionResultProps) {
   return (
-    <div className="flex w-full flex-col justify-center gap-5 rounded bg-gray-50 px-5 py-2 shadow-md shadow-gray-500 dark:border dark:border-gray-800 dark:bg-zinc-900">
+    <div className="flex w-full h-fit flex-col justify-center gap-5 rounded mt-16 md:mt-0 px-5 py-4 border dark:border border-gray-300 dark:border-gray-800 dark:bg-zinc-800" >
       {/* TODO: add animation to load content */}
-      {/* <p className="whitespace-nowrap text-xl text-gray-600 dark:text-gray-400">
-        {prediction &&
-          `The ${prediction.coin} price prediction is $${
-            prediction.prediction
-          } at ${prediction.time.toLocaleTimeString()}. This prediction used the ${
-            prediction.model
-          } model.`}
-        {!prediction && "Set prediction parameters to get started."}
-      </p> */}
+      <p className="text-center text-2xl"><b>{prediction?.coin.startsWith("E") ? "Ethereum" : "Bitcoin"} Prediction</b></p>
       <PredictionTable>
         <PredictionRow
-          title="Prediction"
+          title="Prediction:"
           value={prediction?.prediction ? `$${prediction.prediction}` : "$0"}
           valueColor={prediction?.difference.startsWith("-") ? "red" : "green"}
         />
         <PredictionRow
-          title="Time"
+          title="Time:"
           value={
             prediction?.time
               ? prediction.time.toLocaleTimeString()
-              : "Run Prediction"
+              : "Choose a time"
           }
         />
         <PredictionRow
-          title="Current Price"
+          title="Current Price:"
           value={
             prediction?.current_price ? `$${prediction.current_price}` : "$0"
           }
         />
         <PredictionRow
-          title="Difference"
-          value={prediction?.difference ?? "0"}
+          title="Difference:"
+          value={prediction?.difference ? `$${prediction.difference}` : "$0"}
           valueColor={prediction?.difference.startsWith("-") ? "red" : "green"}
         />
         <PredictionRow
-          title="Model"
+          title="Model:"
           value={prediction?.model?.toUpperCase() ?? "Choose a model"}
         />
       </PredictionTable>

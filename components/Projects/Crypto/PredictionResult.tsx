c0d5e6bc136a877e6b6fa1eaf6ca8b1036c6dcf0
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-14 11:23:15 -0600

    Refactor to default with white text

--- components/Projects/Crypto/PredictionResult.tsx
+++ components/Projects/Crypto/PredictionResult.tsx
@@ -8,14 +8,24 @@ type PredictionResultProps = {
 
 export function PredictionResult({ prediction }: PredictionResultProps) {
   return (
-    <div className="flex w-full h-fit flex-col justify-center gap-5 rounded mt-16 md:mt-0 px-5 py-4 border dark:border border-gray-300 dark:border-gray-800 dark:bg-zinc-800" >
+    <div className="mt-16 flex h-fit w-full flex-col justify-center gap-5 rounded border border-gray-300 px-5 py-4 dark:border dark:border-gray-800 dark:bg-zinc-800 md:mt-0">
       {/* TODO: add animation to load content */}
-      <p className="text-center text-2xl"><b>{prediction?.coin.startsWith("E") ? "Ethereum" : "Bitcoin"} Prediction</b></p>
+      <p className="text-center text-2xl">
+        <b>
+          {prediction?.coin.startsWith("E") ? "Ethereum" : "Bitcoin"} Prediction
+        </b>
+      </p>
       <PredictionTable>
         <PredictionRow
           title="Prediction:"
           value={prediction?.prediction ? `$${prediction.prediction}` : "$0"}
-          valueColor={prediction?.difference.startsWith("-") ? "red" : "green"}
+          valueColor={
+            prediction
+              ? prediction.difference.startsWith("-")
+                ? "red"
+                : "green"
+              : "white"
+          }
         />
         <PredictionRow
           title="Time:"
@@ -33,8 +43,22 @@ export function PredictionResult({ prediction }: PredictionResultProps) {
         />
         <PredictionRow
           title="Difference:"
-          value={prediction?.difference ? `${prediction.difference.slice(0, 1) + "$" + prediction.difference.slice(1)}` : "$0"}
-          valueColor={prediction?.difference.startsWith("-") ? "red" : "green"}
+          value={
+            prediction?.difference
+              ? `${
+                  prediction.difference.slice(0, 1) +
+                  "$" +
+                  prediction.difference.slice(1)
+                }`
+              : "$0"
+          }
+          valueColor={
+            prediction
+              ? prediction.difference.startsWith("-")
+                ? "red"
+                : "green"
+              : "white"
+          }
         />
         <PredictionRow
           title="Model:"

cc43bdcfce31fff4ba0ce765a923b6403e93e6bd
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-21 12:48:49 -0600

    Remove unused class name

--- components/Projects/Crypto/PredictionTable.tsx
+++ components/Projects/Crypto/PredictionTable.tsx
@@ -1,6 +1,6 @@
 export function PredictionTable({ children }: { children?: React.ReactNode }) {
   return (
-    <table className="w-300 mx-auto my-2 table-auto whitespace-nowrap py-2 text-lg">
+    <table className="mx-auto my-2 table-auto whitespace-nowrap py-2 text-lg">
       <tbody>{children}</tbody>
     </table>
   );

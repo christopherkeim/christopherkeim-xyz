05cb720547528b49af176c3254ef833428eef393
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-21 15:00:00 -0600

    Cleaned up and added submitting state

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 type ValidationErrors = {
   name?: string;
@@ -9,13 +9,10 @@ type ValidationErrors = {
 
 export default function Contact() {
   const [name, setName] = useState("");
-  //   const nameRef = useRef<string>("");
   const [email, setEmail] = useState("");
-  //   const emailRef = useRef<string>("");
   const [message, setMessage] = useState("");
-  //   const messageRef = useRef<string>("");
   const [errors, setErrors] = useState<ValidationErrors>({});
-  const [isFormValid, setIsFormValid] = useState<boolean>(false);
+  const [submitting, setSubmitting] = useState<boolean>(false);
 
   function validateForm() {
     const errors: ValidationErrors = {};
@@ -37,18 +34,23 @@ export default function Contact() {
     }
 
     setErrors(errors);
-    setIsFormValid(Object.keys(errors).length === 0);
+    return Object.keys(errors).length === 0;
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    validateForm();
+    const formIsValid = validateForm();
     console.log("Name: ", name);
     console.log("Email: ", email);
     console.log("Message: ", message);
-    if (isFormValid) {
+    console.log(formIsValid);
+    if (formIsValid) {
       // Send the form data to the server here...
-      alert("Form is valid, and ready to send!");
+      console.log("Form is valid, and ready to send!");
+      setSubmitting(true);
+      setTimeout(() => {
+        setSubmitting(false);
+      }, 2000);
     }
   }
   return (
@@ -89,7 +91,6 @@ export default function Contact() {
                     value={name}
                     onChange={(event) => {
                       setName(event.currentTarget.value);
-                      //   nameRef.current = event.currentTarget.value;
                     }}
                   />
                 </div>
@@ -117,7 +118,6 @@ export default function Contact() {
                     value={email}
                     onChange={(event) => {
                       setEmail(event.currentTarget.value);
-                      //   emailRef.current = event.currentTarget.value;
                     }}
                   />
                 </div>
@@ -145,7 +145,6 @@ export default function Contact() {
                     value={message}
                     onChange={(event) => {
                       setMessage(event.currentTarget.value);
-                      //   messageRef.current = event.currentTarget.value;
                     }}
                   ></textarea>
                 </div>
@@ -154,19 +153,43 @@ export default function Contact() {
                 <div className="w-full px-3">
                   <button
                     type="submit"
-                    className="btn flex w-full items-center bg-blue-500 text-white hover:bg-blue-400"
+                    disabled={submitting}
+                    className="btn flex w-full items-center bg-blue-500 text-white hover:bg-blue-400 disabled:bg-gray-300 disabled:text-gray-800"
                   >
-                    <span>Send Message</span>
-                    <svg
-                      className="ml-2 mt-px h-3 w-3 shrink-0"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        className="fill-current"
-                        d="M6.602 11l-.875-.864L9.33 6.534H0v-1.25h9.33L5.727 1.693l.875-.875 5.091 5.091z"
-                      />
-                    </svg>
+                    <span>{submitting ? "Submitting" : "Send Message"}</span>
+                    {submitting ? (
+                      <>
+                        {" "}
+                        <svg
+                          aria-hidden="true"
+                          className="ms-2 h-5 w-5 animate-spin fill-blue-600 text-gray-200 dark:text-gray-900"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span className="sr-only">Submitting...</span>
+                      </>
+                    ) : (
+                      <svg
+                        className="ml-2 mt-px h-3 w-3 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          className="fill-current"
+                          d="M6.602 11l-.875-.864L9.33 6.534H0v-1.25h9.33L5.727 1.693l.875-.875 5.091 5.091z"
+                        />
+                      </svg>
+                    )}
                   </button>
                 </div>
               </div>

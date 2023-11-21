a03c0b7dfbbd1a42b8b3c20311ef3b895988ace9
Christopher Keim <christopher.jl.keim@gmail.com>
2023-11-20 21:56:40 -0500

    Wrapped table children in tbody.

--- components/Projects/Crypto/PredictionTable.tsx
+++ components/Projects/Crypto/PredictionTable.tsx
@@ -1,7 +1,7 @@
 export function PredictionTable({ children }: { children?: React.ReactNode }) {
   return (
-    <table className="mx-auto w-300 table-auto whitespace-nowrap text-lg py-2 my-2">
-      {children}
+    <table className="w-300 mx-auto my-2 table-auto whitespace-nowrap py-2 text-lg">
+      <tbody>{children}</tbody>
     </table>
   );
 }

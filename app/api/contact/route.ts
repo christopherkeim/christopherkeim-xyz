5035b914637530243b6105c3224c5ab7f8ecc62d
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-21 15:36:00 -0600

    Added proper validation for parsed values

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,9 +1,11 @@
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const name = body.name;
-    const email = body.email;
-    const message = body.message;
+    if (!body) {
+      return Response.json({ error: "Invalid request" }, { status: 400 });
+    }
+
+    const { name, email, message } = body;
 
     if (!name) {
       return Response.json({ error: "Name is required" }, { status: 400 });
@@ -13,9 +15,22 @@ export async function POST(request: Request) {
       return Response.json({ error: "Email is required" }, { status: 400 });
     }
 
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      return Response.json({ error: "Email is invalid" }, { status: 400 });
+    }
+
     if (!message) {
       return Response.json({ error: "Message is required" }, { status: 400 });
     }
+
+    if (message.length < 60) {
+      return Response.json(
+        { error: "Message should be at least 60 characters" },
+        { status: 400 },
+      );
+    }
+
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
 
     const data = { serverMessage: "Message received!" };
     return Response.json(data);

bfd413e6d8f5b613722dd646c6870234d3e60d35
Christopher Keim <christopher.jl.keim@gmail.com>
2023-11-20 21:55:59 -0500

    Implemented chart.

--- components/Projects/Crypto/PredictionResult.tsx
+++ components/Projects/Crypto/PredictionResult.tsx
@@ -1,13 +1,84 @@
+"use client";
 import { Prediction } from "./PredictionForm";
 import { PredictionRow } from "./PredictionRow";
 import { PredictionTable } from "./PredictionTable";
 import NonSSRWrapper from "@/components/NonSSRWrapper";
+import { useEffect, useRef } from "react";
+import {
+  Chart,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Colors,
+  Legend,
+  Tooltip,
+  Title,
+  Filler,
+} from "chart.js";
+
+// Register our Chart
+Chart.register(
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Colors,
+  Legend,
+  Tooltip,
+  Title,
+  Filler,
+);
 
 type PredictionResultProps = {
   prediction: Prediction | null;
 };
 
 export function PredictionResult({ prediction }: PredictionResultProps) {
+  const canvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const context = canvas.current;
+    if (!context) return;
+
+    if (prediction) {
+      const options = {
+        type: "line",
+        data: {
+          labels: createHours(prediction.time.getHours()),
+          datasets: [
+            {
+              label: "Charts Title",
+              data: [...prediction.prices_24_hours, prediction.prediction],
+              borderWidth: 1,
+            },
+          ],
+        },
+      };
+
+      const newChart = constructChart(context, options);
+      return () => newChart.destroy();
+    }
+  }, [prediction]);
+
+  function constructChart(context: HTMLCanvasElement, options: any): Chart {
+    return new Chart(context, options);
+  }
+
+  function createHours(timestamp: number) {
+    console.log(timestamp);
+    const hour = new Date(timestamp).getHours();
+
+    const hours: number[] = [];
+
+    for (let i = 1; i < 25; i++) {
+      hours.push((hour + i) % 24);
+    }
+    console.log("Hours are: ", hours);
+    return hours;
+  }
   return (
     <div className="mt-16 flex h-fit w-full flex-col justify-center gap-5 rounded border border-gray-300 px-5 py-4 dark:border dark:border-gray-800 dark:bg-zinc-800 md:mt-0">
       {/* TODO: add animation to load content */}
@@ -67,6 +138,7 @@ export function PredictionResult({ prediction }: PredictionResultProps) {
             value={prediction?.model?.toUpperCase() ?? "Choose a model"}
           />
         </PredictionTable>
+        {prediction && <canvas ref={canvas}></canvas>}
       </NonSSRWrapper>
     </div>
   );

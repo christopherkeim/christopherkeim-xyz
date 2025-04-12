e7ad63120677ea9724d2d5b1e0703b91f9fbb44c
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2025-04-12 17:06:45 -0400

    Added `About` component.

---
+++ components/Home/About.tsx
@@ -0,0 +1,22 @@
+export function About() {
+  return (
+    <section className="space-y-8">
+      <h4 className="h4 font-aspekta text-slate-800 dark:text-slate-100">
+        👋 I'm Chris
+      </h4>
+      <div className="font-medium text-slate-800 dark:text-slate-100">
+        I provide software engineering and data science services for products
+        that harness web and AI technologies.
+      </div>
+      <div className="font-medium text-slate-800 dark:text-slate-100">
+        I work across modalities involving green field R&D, big data, machine
+        learning, deep learning, web microservices, and web APIs using
+        technologies like Python, SQL, Golang, and .NET.
+      </div>
+      <div className="font-medium text-slate-800 dark:text-slate-100">
+        I have experience working across on-premise, cloud, and hybrid
+        technology stacks, from small teams to enterprise environments.
+      </div>
+    </section>
+  );
+}

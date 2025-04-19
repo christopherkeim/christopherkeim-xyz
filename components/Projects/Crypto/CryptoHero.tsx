4c574db0d26e74479c97aa65c850bbe50d01228f
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2025-04-18 23:27:17 -0400

    Removed fade down.

--- components/Projects/Crypto/CryptoHero.tsx
+++ components/Projects/Crypto/CryptoHero.tsx
@@ -5,7 +5,7 @@ export default function CryptoHero() {
   return (
     <section>
       <div className="mx-auto mb-8 flex max-w-6xl flex-col items-center px-4 pb-10 pt-32 text-left sm:px-6 md:gap-12 md:px-12 md:pb-16 md:pt-40 lg:gap-12 lg:px-20">
-        <h1 className="h1 font-black lg:text-5xl" data-aos="fade-down">
+        <h1 className="h1 font-black lg:text-5xl">
           Crypto <span className="text-blue-500">Real Time</span>{" "}
           <span className="text-green-500">Inference</span>
         </h1>

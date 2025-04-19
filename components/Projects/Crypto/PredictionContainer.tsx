4f98988f70dd2c8f63b776012b1cc134c4af2281
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2025-04-18 22:22:09 -0400

    Model loads on component load.

--- components/Projects/Crypto/PredictionContainer.tsx
+++ components/Projects/Crypto/PredictionContainer.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
 import {
   PredictionForm,
   Prediction,
 } from "@/components/Projects/Crypto/PredictionForm";
+import { BTC_CNN_PATH, ETH_CNN_PATH, loadModel } from "./MLE/loadModel";
 import { PredictionResult } from "@/components/Projects/Crypto/PredictionResult";
 
 // Example prediction response:
@@ -22,10 +24,37 @@ import { PredictionResult } from "@/components/Projects/Crypto/PredictionResult"
 
 export function PredictionContainer() {
   const [prediction, setPrediction] = useState<Prediction | null>(null);
+  const [btcCnnModel, setBtcCnnModel] = useState<tf.GraphModel | null>(null);
+  const [ethCnnModel, setEthCnnModel] = useState<tf.GraphModel | null>(null);
+
+  useEffect(() => {
+    if (btcCnnModel === null) {
+      wrapLoadBtcCnnModel();
+    }
+  }, [btcCnnModel]);
+
+  useEffect(() => {
+    if (ethCnnModel === null) {
+      wrapLoadEthCnnModel();
+    }
+  }, [ethCnnModel]);
+
+  async function wrapLoadBtcCnnModel() {
+    const model = await loadModel(BTC_CNN_PATH);
+    setBtcCnnModel(model);
+  }
+  async function wrapLoadEthCnnModel() {
+    const model = await loadModel(ETH_CNN_PATH);
+    setEthCnnModel(model);
+  }
 
   return (
     <>
-      <PredictionForm setPrediction={setPrediction} />
+      <PredictionForm
+        btcCnnModel={btcCnnModel}
+        ethCnnModel={ethCnnModel}
+        setPrediction={setPrediction}
+      />
       <PredictionResult prediction={prediction} />
     </>
   );
